8b014eda55061b67bf0b5e6730d80a9e4af29f40
flytape8490 <rivard8490@gmail.com>
2019-11-04 09:10:04 -0600

    attempt at a complete rewrite
    Rewriting to make the code more memory efficient, and also more closely adhere to pep8.
    This does not improve on the overall compression performance.

--- shitty_lz78.py
+++ shitty_lz78.py
@@ -1,110 +1,131 @@
 #! /usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-# Shitty LZ78: v0.5
+'''shitty_lz78.py: A very bad implementation of the LZ78 algorithm.
 
+Usage:  
+shitty_lz78.py -(c|d) <path> <output_path>
+-c(ompress):    Compresses the file at <path> to <output_path>.
+                If <output_path> is not specified, the file will output
+                to <path>.s78
+-d(ecompress):  Decompresses the file at <path> to <output_path>.
+                If <output_path> is not specified, the result will print
+                to the console.
+'''
 
-def compress_78(data):
-	data = data.decode('latin-1')
-	start = b'\x00\x00'
-	stop = b'\xFF\xFF'
-	stack = ['']
-	output = bytearray(start)
-	i = 0
-	size = len(data)
-	chunks = 1
-	while True:
-		w = 1
-		symbol = str(data[i])
-		while symbol in stack and i + w <= size:  # <= so that things close to the end of a block get indexed correctly
-			symbol = data[i:i+w]
-			w += 1
-		glyph = symbol[-1]
-		if len(symbol) == 1:
-			i += 1
-			if glyph not in stack:
-				output += bytearray((0, ord(glyph)))
-				stack.append(glyph)
-			else:
-				idx = stack.index(glyph)
-				output += bytearray((idx, 0))
-		else:
-			lookup = symbol[:-1]
-			idx = stack.index(lookup)
-			if idx < 255:
-				i += w - 1
-				output += bytearray((idx, ord(glyph)))
-				stack.append(symbol)
-			else:
-				i += 1
-				glyph = symbol[0]
-				stack = ['', glyph]
-				output += stop + start + bytearray((0, ord(glyph)))
-				chunks += 1
-		if i >= size:
-			output += stop
-			break
-	print('\nShitty LZ78\n===========')
-	print('Compression ratio:       {0}% of original size'.format(round(len(output)/size * 100, 2)))
-	if chunks > 1:
-		sizes = [len(i) for i in output.decode('latin-1').split('\xFF\xFF')[:-2]]  # [:-2] cuts off the empty final chunk as well as the partial end-chunk
-		print('Total chunks:            {0}'.format(chunks))
-		print('Average chunk length:    {0} bytes'.format(len(output)//chunks//2))
-		print('Min/Max chunk length:    {0}/{1} bytes'.format(min(sizes)//2, max(sizes)//2))
-	else:
-		print('1 chunk, {0} bytes'.format(size))
+__version__ = 'v1.0'
+__author__ = 'rivard8490 at gmail.com'
+__copyright__ = 'Copyright 2019'
+__todo__ = '* set-up to use the argument module'
 
-	return output
+from sys import argv
+import chunks
+
+stop = b'\xFF\xFF'
+
+def compress_78(path, output_path=None):
+    if output_path is None: output_path = path + '.s78'
+
+    input_file = chunks.read(path, 1, 'rb')
 
 
-def decompress_78(data):
-	assert data[:2] == b'\x00\x00', 'This may not have been compressed with this software - expansion halted'
-	output = bytearray()
-	frames = data[0:-2:2]  # -2 to drop the ending \xFF to reduce code needed for .split replacement
-	glyphs = data[1:-2:2]  # -2 to drop the ending \xFF to reduce code needed for .split replacement
-	size = len(frames)
-	i = 1
-	while i < size:
-		current_frame_content = frames[i]
-		assert current_frame_content != i, 'POTENTIAL ZIP BOMB - EXPANSION HALTED'
-		if current_frame_content == 255:  # this is to replace using .split on the data, which caused issues with certain patterns
-			frames = frames[i+1:]
-			glyphs = glyphs[i+1:]
-			size = len(frames)
-			i = 1
-			continue
-		elif current_frame_content == 0:
-			output.append(glyphs[i])
-		else:
-			work = bytearray()
-			# if glyphs[i] != 0:
-			if i + 1 != size or (i + 1 == size and glyphs[i] != 0):  # this is to catch 0-glyphs that aren't at the end of a block
-				work.append(glyphs[i])
-			pointer = i
-			while frames[pointer] != 0:  # add next glyph
-				# issue with multiple zero reconstruction resolved
-				pointer = frames[pointer]
-				work.append(glyphs[pointer])
-			output += work[::-1]
-		i += 1
-	return bytes(output)
+    with open(output_path, 'xb') as output_file:
+        stack = ['']
+        symbol = bytes()
+        while True:
+            # grow symbol by one character from the input file
+            try:
+                symbol += next(input_file)
+            except StopIteration:
+                if len(symbol) != 0:
+                    output_file.write(bytes((stack.index(symbol),)))
+                break
+
+            # if the symbol is a single character not in the stack
+            if len(symbol) == 1 and symbol not in stack:
+                stack.append(symbol)
+                output_file.write(b'\x00' + symbol)
+                symbol = bytes()
+
+            # if the symbol is line not in the stack
+            elif symbol not in stack:
+                position = stack.index(symbol[:-1])
+                if position < 255:
+                    stack.append(symbol)
+                    block = bytes((position, symbol[-1]))
+                    output_file.write(block)
+                    symbol = bytes()
+                else:
+                    if len(symbol) > 2:
+                        position = bytes((stack.index(symbol[:-2]),))
+                        output_file.write(position + stop)
+                    hold = bytes((symbol[-2],))
+                    stack = ['', hold]
+                    output_file.write(b'\x00' + hold)
+                    symbol = bytes((symbol[-1],))
+                    if symbol != hold:
+                        stack.append(symbol)
+                        output_file.write(b'\x00' + symbol)
+                        symbol = bytes()
+
+
+def decompress_78(path, output_path=False):
+    if output_path is not False: output_file = open(output_path, 'xb')
+
+    input_file = chunks.read(path, 512, 'rb')
+    block = next(input_file)
+
+    while block != b'':
+        output_buffer = bytes()
+
+        while stop not in block:
+            try:
+                block += next(input_file)
+            except StopIteration:
+                break
+
+        block = block.split(stop, 1)
+
+        if len(block) == 1:
+            work, block = block[0], bytes()
+        else:
+            work, block = block
+
+        stack = tuple((tuple(work[i:i+2]) for i in range(0, len(work), 2)))
+
+        for pair in stack:
+            internal_buffer = bytes()
+
+            if len(pair) == 2:
+                lookup, glyph = pair
+                if lookup == 0:
+                    output_buffer += bytes((glyph,))
+                else:
+                    internal_buffer = bytes((glyph,))
+                    while lookup != 0:
+                        lookup, glyph = stack[lookup - 1]
+                        internal_buffer += bytes((glyph,))
+
+            else:  # half-tuple at end of stack
+                lookup = pair[0]
+                while lookup != 0:
+                    lookup, glyph = stack[lookup - 1]
+                    internal_buffer += bytes((glyph,))
+
+            output_buffer += internal_buffer[::-1]
+
+        if output_path is False:
+            print(output_buffer, end='')
+        else:
+            output_file.write(output_buffer)
 
 
 if __name__ == '__main__':
-	# TODO: use sys.stdin to accept from pipe?
-	# clean up args, maybe even use the args library?
-	# redo the open to read files in as chunks, write/append as chunks
-	from sys import argv
-	if '-d' in argv:
-		with open(argv[-2], 'rb') as input:
-			data = input.read()
-		output = open(argv[-1], 'wb')
-		output.write(decompress_78(data))
-		output.close()
-	elif '-c' in argv:
-		with open(argv[-1], 'rb') as input:
-			data = input.read()
-		output = open(argv[-1]+'.s78', 'wb')
-		output.write(compress_78(data))
-		output.close()
-	else:
-		print('(-c)ompress <path>: outputs a compressed file to <path>.s78\n(-d)ecompress <input path> <output path>: takes input and decompresses it to output')
+    if '-d' in argv:  # decompress
+        pass
+    elif '-c' in argv:  # compress
+        compress_78(path, output_path)
+    else:
+        print(__doc__)
+
+        # if ends in ffff, it's the old version of decompress
